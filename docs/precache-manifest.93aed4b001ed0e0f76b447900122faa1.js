0f565879f31a95617691b4c606947127f01f4db1
pofid-boking <pofid-boking@protonmail.com>
2020-09-14 21:37:48 +0800

    update

---
+++ docs/precache-manifest.93aed4b001ed0e0f76b447900122faa1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e0af3b446409840ae5d953e6dae5bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61b5bc22a978da8f2388",
+    "url": "./static/css/2.3ec1430a.chunk.css"
+  },
+  {
+    "revision": "f58840f9360954406c83",
+    "url": "./static/css/main.152c06df.chunk.css"
+  },
+  {
+    "revision": "61b5bc22a978da8f2388",
+    "url": "./static/js/2.2defbd52.chunk.js"
+  },
+  {
+    "revision": "fa3c62d97ab7b6c477c41839164bc26d",
+    "url": "./static/js/2.2defbd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58840f9360954406c83",
+    "url": "./static/js/main.2a7b1eed.chunk.js"
+  },
+  {
+    "revision": "3923ed8ee3f896cb1e34",
+    "url": "./static/js/runtime-main.55dbddb4.js"
+  },
+  {
+    "revision": "63482f8eec30a30c465f2057cb905d3e",
+    "url": "./static/media/header_logo.63482f8e.png"
+  },
+  {
+    "revision": "35b5db7b9d277ca3335c317a33f1a91d",
+    "url": "./static/media/poke.35b5db7b.png"
+  }
+]);
